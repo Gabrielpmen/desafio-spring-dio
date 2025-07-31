0e279c31d28eb4b35713a1ebfcc3a026ebd47037
Gabriel Mendonca <gabriel-peres@hotmail.com>
2025-07-31 01:03:38 -0300

    Adicionando endpoints

--- src/test/java/one/digitalinnovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplicationTests.java
+++ src/test/java/one/digitalinnovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplicationTests.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.lab_padroes_projeto_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class LabPadroesProjetoSpringApplicationTests {
 
